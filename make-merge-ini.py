3c0d1becdc30b3726ec3e60a2e17f7ae8a6d8a3d
Brian Fitzgerald <neurocline@gmail.com>
2021-10-26 16:58:03 -0700

    Collect remotes, worktrees, and commit counts

--- make-merge-ini.py
+++ make-merge-ini.py
@@ -13,10 +13,22 @@ def generate(info):
 
     for entry in info:
         print("")
-        print(f"[repo-{entry['subtree']}]")
+        print(f"[repo.{entry['subtree']}]")
         print(f"source = {entry['source']}")
         print(f"subtree = {entry['subtree']}")
         print(f"main = {entry['branches'][0]}")
+        print(f"commits = {entry['commits']}")
+
+        # Use the pattern ", " to separate items in a list, because space is not
+        # allowed in tags, refnames (branches and remotes) or URLS (remote URLS)
+        if len(entry['branches']) > 0:
+            print(f"branches = \"{', '.join(entry['branches'])}\"")
+        if len(entry['tags']) > 0:
+            print(f"tags = \"{', '.join(entry['tags'])}\"")
+        if len(entry['remotes']) > 0:
+            print(f"remotes = \"{', '.join(entry['remotes'])}\"")
+        if len(entry['worktrees']) > 0:
+            print(f"worktrees = \"{', '.join(entry['worktrees'])}\"")
 
 def gather(root):
     import os
@@ -39,16 +51,19 @@ def gather(root):
 
 def gather_repo(gitpath):
     import os.path
+    import sys
 
     branches = get_git_branches(gitpath)
-    tags = get_git_tags(gitpath)
-    if branches is None or tags is None:
+    if branches is None:
+        print(f"Error (branches): {gitpath}", file=sys.stderr)
         return None
-
-    #if len(branches) != 1:
-    #    return None
-    #if len(tags) != 0:
-    #    return None
+    tags = get_git_tags(gitpath)
+    remotes = get_git_remotes(gitpath)
+    num_commits = get_git_num_commits(gitpath)
+    worktrees = get_git_worktrees(gitpath)
+    if tags is None or remotes is None or num_commits is None:
+        print(f"Error (other): {gitpath}", file=sys.stderr)
+        return None
 
     subtree = os.path.basename(gitpath)
 
@@ -56,7 +71,10 @@ def gather_repo(gitpath):
         'source': gitpath,
         'subtree': subtree,
         'branches': branches,
-        'tags': tags
+        'tags': tags,
+        'remotes': remotes,
+        'commits': num_commits[0],
+        'worktrees': worktrees
     }
     return item
 
@@ -71,6 +89,43 @@ def get_git_branches(gitpath):
 
 def get_git_tags(gitpath):
     return run_get([gitpath, "tag", "--list"])
+
+def get_git_remotes(gitpath):
+    output = run_get([gitpath, "remote"])
+
+    remotes = []
+    for remote_name in output:
+        remote = run_get([gitpath, "remote", "get-url", remote_name])
+        if remote is not None:
+            remotes.append(f"{remote_name}={remote[0]}")
+    return remotes
+
+def get_git_num_commits(gitpath):
+    return run_get([gitpath, "rev-list", "--all", "--count"])
+
+def get_git_worktrees(gitpath):
+    import re
+    import os.path
+
+    # Don't bother to return the built-in worktree. And note that it's atypical for someone
+    # to have worktrees. We'd like to know, because it's easy to lose track of them.
+    output = run_get([gitpath, "worktree", "list"])
+    re_worktree = re.compile(r'(.+) ([a-fA-F0-9]{7,}) \[([^]]+)\]( prunable)?')
+
+    worktrees = []
+    for line in output:
+        m = re_worktree.fullmatch(line)
+        if m is None:
+            raise RuntimeError(f"failed to match: {output}")
+        worktree_path = m.group(1).rstrip()
+        worktree_hash = m.group(2)
+        worktree_branch = m.group(3)
+        if worktree_path.lower() == os.path.abspath(gitpath).lower().replace("\\", "/"):
+            # print(f"Skipping {worktree_path} because this is {gitpath}")
+            pass
+        else:
+            worktrees.append(f"{worktree_branch}:{worktree_hash}:{worktree_path}")
+    return worktrees
 
 def run_get(cmd):
     import subprocess
